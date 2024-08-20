4583e1e8899016580936f7fe191dad9a715937ae
arikru <krumbach.arian@gmail.com>
2024-08-20 21:17:25 +0200

    draw to screen

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+
+from constants import *
+
+
+def main():
+    pygame.init()
+    print("Starting asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
