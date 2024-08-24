a8072888acc4eb54962661166126ee701992fa1b
arikru <krumbach.arian@gmail.com>
2024-08-24 11:31:01 +0200

    checkpoint FPS

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,6 +18,7 @@ def main():
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000  # convert from milliseconds to seconds
 
 
 if __name__ == "__main__":
