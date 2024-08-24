d9fa0ea936b42133f231012b467ee31d09138eb7
arikru <krumbach.arian@gmail.com>
2024-08-24 15:26:43 +0200

    checkpoint groups

--- main.py
+++ main.py
@@ -14,15 +14,26 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
     player = Player(x, y, PLAYER_RADIUS)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         pygame.Surface.fill(screen, (0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+
+        for member in updateable:
+            member.update(dt)
+        for member in drawable:
+            member.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000  # convert from milliseconds to seconds
 
